4e4cb5147ecd699d6675f817887f365690d873db
Connor Mendenhall <ecmendenhall@gmail.com>
2012-08-29 14:00:51 -0700

    Started implementing logins. Hooked up Transcript and Edit views.

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), "lib/bcrypt"))
 
 from google.appengine.ext import db
 from google.appengine.ext.webapp import template
-from models import User
+from models import User, Settings
 import jinja2
 import webapp2
 import os
@@ -11,6 +14,7 @@ import re
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
                                autoescape = True)
+
 
 class CustomHandler(webapp2.RequestHandler):
     def write(self, *a, **kw):
@@ -30,7 +34,7 @@ class SignInHandler(CustomHandler):
         return username and user_re.match(username)
     
     def valid_password(self, password):
-        pass_re = re.compile(r"^.{3,25}$")
+        pass_re = re.compile(r"^.{8,25}$")
         return password and pass_re.match(password)
 
     def valid_email(self, email):
@@ -59,6 +63,12 @@ class SignUp(SignInHandler):
 
         if not self.valid_username(self.username):
             params['username_error'] = "That's not a valid username."
+            error = True
+
+        #Check if user already exists
+        u = User.by_name(self.username)
+        if u:
+            params['username_error'] = 'That user already exists.'
             error = True
 
         if not self.valid_password(self.password):
@@ -96,30 +106,90 @@ class FinishSignUp(SignInHandler):
             self.done()
 
     def done(self):
-        #Check if user already exists
-        u = User.by_name(self.username)
-        if u:
-            username_error = 'That user already exists.'
-            self.render('signup.html', username_error = username_error)
-        else:
-            u = User.register(self.username, self.password, self.email)
-            u.put()
-
-            self.login(u)
-            self.redirect('/')
+        User.register(self.username, self.password, self.email)
+        
+        #self.login(u)
+        self.redirect('/' + self.username)
 
 
 class Edit(CustomHandler):
-    def get(self):
-        self.response.out.write('Hello world!')
+    def get(self, username):
+
+        #Check if user exists
+        u = User.by_name(username)
+        if not u:
+            self.response.out.write('404 Not Found')
+
+        else:
+            params = { 'name': u.name,
+                       'twitter': u.twitter,
+                       'github': u.github,
+                       'website': u.website }
+            
+            s = Settings.by_username(username)
+            if s:
+                params['linkcolor'] = s.link_color
+                params['sidebarcolor'] = s.sidebar_color
+                params['headercolor'] = s.header_color
+                params['headerfont'] = s.header_font
+                params['bodyfont'] = s.body_font 
+
+            self.render('edit.html', **params)
+
+    def post(self, username):
+        self.name = self.request.get('name')
+        self.twitter = self.request.get('twitter')
+        self.github = self.request.get('github')
+        self.website = self.request.get('website')
+        
+        u = User.by_name(username)
+        u.name = self.name
+        u.twitter = self.twitter
+        u.github = self.github
+        u.website = self.website
+        u.put()
+
+        self.linkcolor = self.request.get('linkcolor')
+        self.sidebarcolor = self.request.get('sidebarcolor')
+        self.headercolor = self.request.get('headercolor')
+        self.headerfont = self.request.get('headerfont')
+        self.bodyfont = self.request.get('bodyfont')
+        
+        s = Settings.by_username(username)
+        s.link_color = self.linkcolor
+        s.sidebar_color = self.sidebarcolor
+        s.header_color = self.headercolor
+        s.header_font = self.headerfont
+        s.body_font = self.bodyfont
+        s.put()
+
+
+        
+        self.redirect('/' + username)
+
+      
 
 class Transcript(CustomHandler):
-    def get(self):
-        self.response.out.write('This is a transcript page!')
+    def get(self, username):
+        
+        #Check if user exists
+        u = User.by_name(username)
+        if not u:
+            self.response.out.write('404 Not Found')
 
-app = webapp2.WSGIApplication([('/', Main),
-                               ('/signin', SignIn),
-                               ('/signup', SignUp),
-                               ('/finish', FinishSignUp),
-                               ('/transcript', Transcript)],
+        else:
+            params = { 'username': u.username,
+                       'name': u.name,
+                       'twitter': u.twitter,
+                       'github': u.github,
+                       'website': u.website }
+            self.render('transcript.html', **params)
+
+
+app = webapp2.WSGIApplication([webapp2.Route('/', Main),
+                               webapp2.Route('/signin', SignIn),
+                               webapp2.Route('/signup', SignUp),
+                               webapp2.Route('/finish', FinishSignUp),
+                               webapp2.Route('/<username>', Transcript),
+                               webapp2.Route('/<username>/edit', Edit)],
                               debug=True)
--- models.py
+++ models.py
@@ -7,7 +7,6 @@ def make_pw_hash(username, pw, salt = None):
     h = bcrypt.hashpw(username + pw, salt)
     return '%s,%s' % (h, salt)
 
-    
 def valid_pw(username, pw, h):
     return h == bcrypt.hashpw(username + pw, h.split(',')[1])
 
@@ -17,10 +16,11 @@ def users_key(group = 'default'):
 class User(db.Model):
     username = db.StringProperty(required=True)
     email = db.EmailProperty()
-    pw_hash = db.StringProperty()
+    pw_hash = db.StringProperty(required=True)
     name = db.StringProperty()
     github = db.StringProperty()
     twitter = db.StringProperty()
+    website = db.StringProperty()
 
     @classmethod
     def by_id(cls, uid):
@@ -34,11 +34,14 @@ class User(db.Model):
     @classmethod
     def register(cls, username, pw, email = None):
         pw_hash = make_pw_hash(username, pw)
-        return User(parent = users_key(),
+        u = User(parent = users_key(),
                     username = username,
                     pw_hash = pw_hash,
                     email = email)
-
+        u.put() 
+        s = Settings(user = u.key())
+        s.put()
+        
     @classmethod
     def login(cls, username, pw):
         u = cls.by_name(username)
@@ -73,4 +76,10 @@ class Settings(db.Model):
     header_color = db.StringProperty()
     header_font = db.StringProperty()
     body_font = db.StringProperty()
-    owner = db.ReferenceProperty(User)
+    user = db.ReferenceProperty(User, required=True)
+
+    @classmethod
+    def by_username(cls, username):
+        u = User.all().filter('username =', username).get()
+        s = Settings.all().filter('user =', u.key()).get()
+        return s
